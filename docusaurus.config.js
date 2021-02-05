c359bda422e287e66879b45ed1751acae9cf4589
iam-kevin <kevin.al.james@gmail.com>
2021-02-05 17:28:01 +0300

    Initial Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  title: 'Nena API',
+  tagline: 'Get to build swahili first tools with Nena',
+  url: 'https://nena-docs.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'inspiredideas', // Usually your GitHub org/user name.
+  projectName: 'nena-docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog/', label: 'Blog', position: 'left'},
+      ],
+    },
+    footer: {
+      // style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting started with Nena',
+              to: 'docs/getting-started',
+            },
+            {
+              label: 'API Reference',
+              to: 'docs/api-reference',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Facebook',
+              href: 'https://facebook.com/maryforafrica',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/maryforafrica',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Nena Dashboard (Coming soon)',
+              to: '#nena-coming-soon',
+            },
+            {
+              label: 'Mary.Africa',
+              to: 'https://mary.africa',
+            },
+            {
+              label: 'Inspired Ideas',
+              to: 'https://inspiredideas.io',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright &copy; ${new Date().getFullYear()} Nena, a Mary.Africa project. All rights reserved`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/inspiredideas/nena-docs/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/inspiredideas/nena-docs/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
